8db2f7aa6bc2daa21c052747410b77f2f01e327c
DonKingFrog <65225182+DonKingFrog@users.noreply.github.com>
2023-11-27 09:58:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("The JS File Ran")
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', () => {
+    console.log('The bot is online!');
+});
+
+client.login(secret.TOKEN);
