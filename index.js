36d9f9c4156a0077c0c8638a5280613e2b3cb30b
DonKingFrog <65225182+DonKingFrog@users.noreply.github.com>
2023-11-27 09:52:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv/config');
-
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-});
-
-client.on('ready', () => {
-    console.log('The bot is online!');
-    client.user.setActivity('Don develop me', { type: 'WATCHING' })
-});
-
-client.login(secret.TOKEN);
+console.log("The JS File Ran")
